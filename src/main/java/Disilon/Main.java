c2e7d67909408c2abfbdc8e008e421e0d62f5dae
Disilon <disilon@gmail.com>
2025-03-10 01:54:24 +0500

    Trying to compile it to exe, no luck Jar works

--- src/main/java/Disilon/Main.java
+++ src/main/java/Disilon/Main.java
@@ -4,7 +4,11 @@ public class Main {
     public static int game_version = 1531;
 
     public static void main(String[] args) {
-        UserForm uf = new UserForm();
+        try {
+            UserForm uf = new UserForm();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
